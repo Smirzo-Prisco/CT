6e3f66cb821b5dda43058667caa82ce1b5b213cd
Roberto Bottini <redito@hotmail.it>
2026-05-12 22:49:35 +0200

    Se entro ed esco, funziona. Se cambio luogo, no. Sto cercando di fixare

--- pages/frame_online.inc.php
+++ pages/frame_online.inc.php
@@ -1,14 +1,8 @@
-<?php include('../ref_header.inc.php'); /*Header comune*/ ?>
-
 <div class="pagina_presenti">
     <div class="page_title">
         <h2><?php echo gdrcd_filter('out', $MESSAGE['interface']['logged_users']['plur']); ?></h2>
     </div>
-    
-    <!-- 🎯 SOSTITUISCI IL CODICE PHP CON QUESTO CONTAINER VUOTO -->
     <div id="online-users-container">
         <div class="loading">Caricamento utenti online...</div>
     </div>
 </div>
-
-<?php include('../footer.inc.php');  /*Footer comune*/ ?>
