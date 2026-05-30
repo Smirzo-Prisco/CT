a0344052b283c36f45e1d7e8f862e0fdd88e62af
Roberto Bottini <redito@hotmail.it>
2026-05-30 18:28:25 +0200

    Debug: test includes

--- pages/api_mestiere.php
+++ pages/api_mestiere.php
@@ -1,2 +1,7 @@
 <?php
-echo json_encode(['test' => 'ok']);
+session_start();
+header('Content-Type: application/json');
+require_once(__DIR__ . '/../includes/required.php');
+require_once(__DIR__ . '/../includes/custom_functions.inc.php');
+gdrcd_connect();
+echo json_encode(['test' => 'includes ok', 'login' => $_SESSION['login'] ?? 'no session']);
