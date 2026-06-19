dcfd92847fc1be9534f628a05dbbc6da500235f2
Roberto Bottini <redito@hotmail.it>
2026-06-19 10:36:48 +0200

    Cleanup: rimozione file inutilizzati (legacy, debug, orfani)

--- check_ip.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$_SERVER['REMOTE_ADDR'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
-$IP = $_SERVER['REMOTE_ADDR'];
-$Host = gethostbyaddr($_SERVER['REMOTE_ADDR']);
-
-echo "your IP is $IP and your host is $Host";
-
-?>
